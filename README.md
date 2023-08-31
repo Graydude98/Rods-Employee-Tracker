16d5860aeeefa97a73113f99b5b710a1a2250ff8
Graydude98 <rodericksimani@yahoo.com>
2023-08-31 15:11:18 -0400

    added more vars for app.get and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Rods-Employee-Tracker
 This App will be used to track any employee no matter what kind of field they're in
+Getting app started
+- open xampp app and then start apache and mysql
+- open terminal
+- type node server.js
+- then in your preferred browser type localhost:3000/. After the "/" you can type createdb, deleteeployee, updateemployee etc.
--- server.js
+++ server.js
@@ -3,17 +3,18 @@ const mysql = require("mysql");
 
 // Create connection
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password:"", 
-})
+    host: 'localhost',
+    user: 'root',
+    password: "",
+    database: 'nodemysql',
+});
 
 // Connect to MySQL
 db.connect(err => {
-    if(err) {
+    if (err) {
         throw err;
     }
-    console.log("MySQL Connected");
+    console.log('MySQL Connected');
 });
 
 const app = express();
@@ -22,9 +23,71 @@ const app = express();
 app.get("/createdb", (req, res) => {
     let sql = "CREATE DATABASE nodemysql";
     db.query(sql, (err) => {
-        if(err) {
+        if (err) {
             throw err;
         }
-        res.send("Database Created")
+        res.send("Database Created");
     });
 });
+
+// Create Table
+app.get('/createemployee', (req, res) => {
+    let sql = 'CREATE TABLE employee(id int AUTO_INCREMENT, name VARCHAR(255), designation VARCHAR(255), PRIMARY(id))'
+    db.query(sql, err => {
+        if (err) {
+            throw err
+        }
+        res.send('Employee table created')
+    })
+})
+
+// Insert employee
+app.get('/employee1', (req, res) => {
+    let post = { name: 'Agent Smith', designation: 'Chief Executive Officer' }
+    let sql = 'INSERT INTO employee SET?'
+    let query = db.query(sql, post, err => {
+        if (err) {
+            throw err
+        }
+        res.send('Employee added')
+    })
+})
+
+// select employees
+app.get('/getemployee', (req, res) => {
+    let sql = 'SELECT * FROM employee'
+    let query = db.query(sql, (err, results) => {
+        if (err) {
+            throw err
+        }
+        console.log(results)
+        res.send('Employee details fetched')
+    })
+})
+
+//update employee
+app.get('/updateemployee/:id', (req, res) => {
+    let newName = 'Updated name'
+    let sql = `UPDATE employee SET name = '${newName}' WHERE id = ${req.params.id}`
+    let query = db.query(sql, err => {
+        if (err) {
+            throw err
+        }
+        res.send('Employee updated')
+    })
+})
+
+// delete employee
+app.get('/deleteemployee/:id', (req, res) => {
+    let sql = `DELETE FROM employee WHERE id = ${req.params.id}`
+    let query = db.query(sql, err => {
+        if (err) {
+            throw err
+        }
+        res.send('Employee deleted')
+    })
+})
+
+app.listen('3000', () => {
+    console.log('Server Started on port 3000')
+})
